070810e259fda761b20ef48443a898b0a0c64672
Ensar Karamürteza <ensarkara13@gmail.com>
2021-12-12 23:31:33 +0300

    ejs template engine added

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  // res.send(blog);
+  res.render('index');
 });
 
-const port = 3000;
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.listen(port);
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda aktif!!!`);
+});
