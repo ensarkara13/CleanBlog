8bc6a25fcd214bcfe6f642bbb8d4e970b92d1955
Ensar Karamürteza <ensarkara13@gmail.com>
2021-12-15 16:47:20 +0300

    delete and update actions added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ejs = require('ejs');
+const methodOverride = require('method-override');
+// const ejs = require('ejs');
 
-const Post = require('./models/Post');
+const {
+  getAboutPage,
+  getAddPostPage,
+  getEditPage,
+} = require('./controllers/pageController');
+const {
+  getAllPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+} = require('./controllers/postController');
 
 const app = express();
 
@@ -16,35 +28,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 //Routes
-app.get('/', async (req, res) => {
-  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  // res.send(blog);
-  const posts = await Post.find();
-  res.render('index', { posts });
-});
+app.get('/', getAllPosts);
+app.get('/posts/:id', getPost);
+app.post('/posts', createPost);
+app.put('/posts/:id', updatePost);
+app.delete('/posts/:id', deletePost);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', { post });
-});
-
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/about', getAboutPage);
+app.get('/add_post', getAddPostPage);
+app.get('/posts/edit/:id', getEditPage);
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda aktif!!!`);
+  console.log(`Server is on port ${port}`);
 });
